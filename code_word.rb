26e626fe64fc09cdcc7c5d55aa8cfcf105794547
RandyCoffman <randy.coffman@wvjc.edu>
2018-03-13 14:57:47 -0400

    working function

--- code_word.rb
+++ code_word.rb
@@ -1,3 +1,10 @@
 def code_word(error)
-	"Page wasn't found."
+	message = ""
+	error_hash = { 404 => "Page wasn't found.", 1942 => "grandpa was found", 402 => "Page almost found"} 
+	error_hash.each_key { |key|
+		if key == error
+			message = error_hash[key]
+		end
+	}
+	message
 end
